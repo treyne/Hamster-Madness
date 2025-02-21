22e9e11a932fef38cf8732bcd3909f6c90be8321
Ilya <treyne@mail.ru>
2025-02-22 00:45:13 +0300

    Update headers.py

--- headers.py
+++ headers.py
@@ -1,11 +1,11 @@
 
-def get_headers_opt():
+def get_headers_opt(Method):
     return {
         "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36",
         "Accept": "*/*",
         "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
         "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Access-Control-Request-Method": "POST",
+        f"Access-Control-Request-Method": Method,
         "Access-Control-Request-Headers": "authorization,content-type",
         "Referer": "https://hamsterkombatgame.io/",
         "Connection": "keep-alive",
