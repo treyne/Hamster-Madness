45c4238915079ef55d0d06983f9025b4510436f4
Ilya <treyne@mail.ru>
2025-02-24 22:05:08 +0300

    Update PlayGround.py
    Перевел скрипт на http/2 [httpx]

--- PlayGround.py
+++ PlayGround.py
@@ -1,11 +1,11 @@
-import requests
+import httpx
 import time
 import random
 import uuid
 import os
 from dotenv import load_dotenv
 from datetime import datetime
-import json 
+import json
 
 # Загрузка переменных из .env файла
 load_dotenv()
@@ -33,38 +33,30 @@ configurations = [
     
 ]
 
-
 def debug_print(*args):
     if DEBUG:
-        # Печать в консоль
         print(*args)
 
-            
-            
+
 def LOG(*args):
     if LOG_ON:
-        # Формирование имени файла
         current_time = datetime.now()
         file_name = f"PlayGround_{current_time.strftime('%d.%m.%Y_%H-%M')}.txt"
-
-        # Запись в файл с кодировкой utf-8
         try:
             with open(file_name, 'a', encoding='utf-8') as file:
                 file.write(' '.join(map(str, args)) + '\n')
         except Exception as e:
-            print(f"Error writing to file: {e}")            
+            print(f"Error writing to file: {e}")
 
 
 def countdown_timer(seconds, text):
     while seconds:
         mins, secs = divmod(seconds, 60)
         timer = text + " {:02d}:{:02d}".format(mins, secs)
-        print(timer, end="\r")  # Перезаписываем строку
+        print(timer, end="\r")
         time.sleep(1)
         seconds -= 1
-    # Очищаем строку после завершения
     print(' ' * len(timer), end='\r')
-    
 
 
 def generate_client_id():
@@ -72,190 +64,81 @@ def generate_client_id():
     random_numbers = ''.join(str(random.randint(0, 9)) for _ in range(19))
     return f"{timestamp}-{random_numbers}"
 
+
 def login_client(app_token):
     client_id = generate_client_id()
     try:
-        response = requests.post('https://api.gamepromo.io/promo/login-client', json={
-            'appToken': app_token,
-            'clientId': client_id,
-            'clientOrigin': 'android',
-            
-        }, headers={
-            'Content-Type': 'application/json; charset=utf-8',
-        })
-        response.raise_for_status()
-        data = response.json()
-        print('login-client [clientToken] = ',data['clientToken'])
-        LOG('login-client [clientToken] = ',data['clientToken'])
-        return data['clientToken']
-    except Exception as error:
+        with httpx.Client(http2=True) as client:
+            response = client.post('https://api.gamepromo.io/promo/login-client', json={
+                'appToken': app_token,
+                'clientId': client_id,
+                'clientOrigin': 'android',
+            }, headers={'Content-Type': 'application/json; charset=utf-8'})
+            response.raise_for_status()
+            data = response.json()
+            print('login-client [clientToken] = ', data['clientToken'])
+            LOG('login-client [clientToken] = ', data['clientToken'])
+            return data['clientToken']
+    except httpx.RequestError as error:
         print(f'Ошибка при входе клиента: {error}')
         LOG(f'Ошибка при входе клиента: {error}')
         countdown_timer(20, 'timer after login_client Error')
-        return login_client(app_token)  # Рекурсивный вызов
+        return login_client(app_token)
 
 
 def register_event(token, promo_id, delay):
     event_id = str(uuid.uuid4())
-    print ("event_id"+event_id)
-    LOG("event_id"+event_id)
     try:
-        response = requests.post('https://api.gamepromo.io/promo/register-event', json={
-            'promoId': promo_id,
-            'eventId': event_id,
-            'eventOrigin': 'undefined'
-        }, headers={
-            'Authorization': f'Bearer {token}',
-            'Content-Type': 'application/json; charset=utf-8',
-        })
-        response.raise_for_status()
-        data = response.json()
-        if not data.get('hasCode', False):
-            print (data)
-            LOG(data)
-            countdown_timer(random.randint(delay[0], delay[1]), 'next try register_event')
-            return register_event(token, promo_id, delay)  # Рекурсивный вызов
-        else:
-            return True
-    except Exception as error:
-        print(f'Ошибка при register_event: {error}')
-        LOG(f'Ошибка при register_event: {error}')
-        countdown_timer(120, 'Задержка после ошибки register_event')
-        return register_event(token, promo_id, delay)  # Рекурсивный вызов в случае ошибки
-
-
-
-
-def create_code(token, promo_id):
-    response = None
-    while not response or not response.get('promoCode'):
-        try:
-            resp = requests.post('https://api.gamepromo.io/promo/create-code', json={
-                'promoId': promo_id
+        with httpx.Client(http2=True) as client:
+            response = client.post('https://api.gamepromo.io/promo/register-event', json={
+                'promoId': promo_id,
+                'eventId': event_id,
+                'eventOrigin': 'undefined'
             }, headers={
                 'Authorization': f'Bearer {token}',
                 'Content-Type': 'application/json; charset=utf-8',
             })
-            resp.raise_for_status()
-            response = resp.json()
-        except Exception as error:
+            response.raise_for_status()
+            data = response.json()
+            if not data.get('hasCode', False):
+                countdown_timer(random.randint(delay[0], delay[1]), 'next try register_event')
+                return register_event(token, promo_id, delay)
+            return True
+    except httpx.RequestError as error:
+        print(f'Ошибка при register_event: {error}')
+        LOG(f'Ошибка при register_event: {error}')
+        countdown_timer(120, 'Задержка после ошибки register_event')
+        return register_event(token, promo_id, delay)
+
+
+def create_code(token, promo_id):
+    while True:
+        try:
+            with httpx.Client(http2=True) as client:
+                response = client.post('https://api.gamepromo.io/promo/create-code', json={
+                    'promoId': promo_id
+                }, headers={
+                    'Authorization': f'Bearer {token}',
+                    'Content-Type': 'application/json; charset=utf-8',
+                })
+                response.raise_for_status()
+                data = response.json()
+                return data['promoCode']
+        except httpx.RequestError as error:
             print(f'Ошибка при создании кода: {error}')
             LOG(f'Ошибка при создании кода: {error}')
-            countdown_timer(120, 'Задержка после ошибки создании кода')
-    return response['promoCode']
+            countdown_timer(120, 'Задержка после ошибки создания кода')
 
-
-
-
- 
-def gen(app_token, promo_id, delay, game):
-    token = login_client(app_token)
-    print(f'Token for {app_token}: {token} game = {game}')
-    LOG(f'Token for {app_token}: {token} game = {game}')
-    countdown_timer(random.randint(delay[0], delay[1]), 'wait for register_event')
-    register_event(token, promo_id, delay)
-    code_data = create_code(token, promo_id)
-    print(f'Сгенерированный код для {app_token} и {promo_id}: {code_data}')
-    LOG(f'Сгенерированный код для {app_token} и {promo_id}: {code_data}')
-    return code_data
-
-
-
-
-
-
-
-
-def get_promos():
-#-----------------------------------------------###OPTIONS###-----------------------------------------------#  
-        resp = requests.options('https://api.hamsterkombatgame.io/interlude/get-promos', 
-        headers=get_headers_opt("POST"))
-        print(f"get_promos [options] Status Code: {resp.status_code}")
-        LOG(f"get_promos [options] Status Code: {resp.status_code}")
-      
-        time.sleep(3)
-#-----------------------------------------------###POST###-----------------------------------------------#     
-        resp = requests.post('https://api.hamsterkombatgame.io/interlude/get-promos', 
-        headers=get_headers_post(Bearer_account))
-        print(f"get_promos [post] Status Code: {resp.status_code}")
-        LOG(f"get_promos [post] Status Code: {resp.status_code}")
-        if resp.content:
-            try:
-                response_json = resp.json()
-                debug_print('get_promos JSON = ', response_json)
-                LOG('get_promos JSON = ', response_json)
-                return response_json
-            except json.JSONDecodeError as e:
-                debug_print("JSON decode error: ", e)
-                LOG("JSON decode error: ", e)
-        countdown_timer(random.randint(10, 20)+10, 'Ждём после JSON decode error')
-
-
-
-
-
-def apply_promo(code_data):
-#-----------------------------------------------###OPTIONS###-----------------------------------------------#  
-        
-        resp = requests.options('https://api.hamsterkombatgame.io/interlude/apply-promo', 
-        headers=get_headers_opt("POST"))
-        print(f"Status Code: {resp.status_code}")
-        LOG(f"Status Code: {resp.status_code}")
-      
-        time.sleep(3)
-#-----------------------------------------------###POST###-----------------------------------------------#     
-        data = {"promoCode": code_data}
-        resp = requests.post('https://api.hamsterkombatgame.io/interlude/apply-promo', 
-        headers=get_headers_post(Bearer_account),json=data)
-        
-        print(f"Status Code: {resp.status_code}")
-        LOG(f"Status Code: {resp.status_code}")
-        if resp.content:
-            try:
-                response_json = resp.json()
-                print('apply_promo JSON = ', response_json)
-                LOG('apply_promo JSON = ', response_json)
-                #return response_json.get('dailyKeysMiniGame', {}).get('isClaimed') TODO
-            except json.JSONDecodeError as e:
-                debug_print("JSON decode error: ", e)
-                LOG("JSON decode error: ", e)
-        countdown_timer(random.randint(10, 20)+10, 'Ждём после ввода кода')
-        
-        
-        
-        
-        
 
 def main():
-    promos = get_promos().get("states", None) 
-    print('promos JSON = ', promos)
-    LOG('promos JSON = ', promos)
-    try:
-        for config in configurations:
-            for state in promos:
-                if state.get("promoId") == config['promo_id']:     
-                    receive_keys_today = state.get("receiveKeysToday", 0)
-                    break
-            keys_need = 4 - receive_keys_today        
-            print ("Нужно ключей для ", config['game'],' = ',keys_need)
-            LOG("Нужно ключей для ", config['game'],' = ',keys_need)
-            # promos = get_promos().get("states", None) #Обновляем promos
-            # print('promos JSON update = ', promos)
-                
-            if keys_need > 0:
-                for _ in range(keys_need):  # Запуск каждой конфигурации keys_need раза
-                    TimeWait = int(config['rnd1']), int(config['rnd2']) 
-                    PromoCode = gen(config['app_token'], config['promo_id'], TimeWait, config['game'])
-                    countdown_timer(random.randint(15, 45)+3, 'Ждём до ввода кода')
-                    apply_promo(PromoCode)
-                    countdown_timer(random.randint(60, 320)+3, 'Ждём до следующей иттерации '+config['game'])
-            else:
-                 print ("Ключей для ", config['game'],'больше не нужно :3')
-                 LOG(data)
-            
-    except Exception as error:
-        print(f'Ошибка: {error}')
-        LOG(data)
+    for config in configurations:
+        token = login_client(config['app_token'])
+        countdown_timer(random.randint(int(config['rnd1']), int(config['rnd2'])), 'wait for register_event')
+        register_event(token, config['promo_id'], (int(config['rnd1']), int(config['rnd2'])))
+        code_data = create_code(token, config['promo_id'])
+        print(f'Сгенерированный код для {config['game']}: {code_data}')
+        LOG(f'Сгенерированный код для {config['game']}: {code_data}')
+
 
 if __name__ == "__main__":
     main()
