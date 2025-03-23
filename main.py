71557e0c0b5ad73f25a86220c35f06f4bb182f70
Ilya <treyne@mail.ru>
2025-03-23 12:00:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,55 +14,45 @@ def main():
         try:
             logger.info(f"[--------->Начата новая иттерация<---------]")
             IP = core.IP()
-            logger.info(f"Ваш IP: <red>{IP["ip"]}</red> | Country: <blue>{IP["country_code"]}</blue> | City: <green>{IP["city_name"]}</green>")
+            logger.info(f"Ваш IP: <red>{IP['ip']}</red> | Country: <blue>{IP['country_code']}</blue> | City: <green>{IP['city_name']}</green>")
             AccountInfo = core.account_info()
-            logger.info(f"Ваш аккаунт ID: <blue>{AccountInfo["accountInfo"]["id"]}</blue> | Name: <blue>{AccountInfo["accountInfo"]["name"]}</blue> | at: <blue>{AccountInfo["accountInfo"]["at"]}</blue>")
+            logger.info(f"Ваш аккаунт ID: <blue>{AccountInfo['accountInfo']['id']}</blue> | Name: <blue>{AccountInfo['accountInfo']['name']}</blue> | at: <blue>{AccountInfo['accountInfo']['at']}</blue>")
             season2_config_version,user = core.sync()
 
-            logger.info(f"gamepads: <blue>{user["user"]["profile"]["gamepads"]}</blue> | gamepads Toltal: <blue>{user["user"]["profile"]["gamepads_total"]}</blue> | reputation: <blue>{user["user"]["profile"]["reputation"]}</blue>")
-            logger.info(f"soft money total: <fg #FFD700>{user["user"]["profile"]["soft_money_total"]}</fg #FFD700> | soft money: <fg #FFD700>{user["user"]["profile"]["soft_money"]}</fg #FFD700>")
-            logger.info(f"hard money total: <fg #FFD700>{user["user"]["profile"]["hard_money_total"]}</fg #FFD700> | hard money: <fg #FFD700>{user["user"]["profile"]["hard_money"]}</fg #FFD700>")
-            logger.info(f"Total Profit: <yellow>{user["user"]["statistics"]["totalProfit"]}</yellow> | totalFun: <yellow>{user["user"]["statistics"]["totalFun"]}</yellow>| totalGames: <yellow>{user["user"]["statistics"]["totalGames"]}</yellow>")
+            logger.info(f"gamepads: <blue>{user['user']['profile']['gamepads']}</blue> | gamepads Toltal: <blue>{user['user']['profile']['gamepads_total']}</blue> | reputation: <blue>{user['user']['profile']['reputation']}</blue>")
+            logger.info(f"soft money total: <fg #FFD700>{user['user']['profile']['soft_money_total']}</fg #FFD700> | soft money: <fg #FFD700>{user['user']['profile']['soft_money']}</fg #FFD700>")
+            logger.info(f"hard money total: <fg #FFD700>{user['user']['profile']['hard_money_total']}</fg #FFD700> | hard money: <fg #FFD700>{user['user']['profile']['hard_money']}</fg #FFD700>")
+            logger.info(f"Total Profit: <yellow>{user['user']['statistics']['totalProfit']}</yellow> | totalFun: <yellow>{user['user']['statistics']['totalFun']}</yellow>| totalGames: <yellow>{user['user']['statistics']['totalGames']}</yellow>")
             game_cfg = core.game_config(season2_config_version)
             if game_cfg:
                  logger.success(f"Файл конфигурации <green>{season2_config_version}.json</green> загружен")
             
-            if user["user"]["counters"]["dailyRewardCounter"]["count"] == 1:
-                ClaimDailyRewards = core.command({"command":{"type":"ClaimDailyRewards"}})
-                if ClaimDailyRewards[1] == 200:  
-                    logger.success(f"Получили ежедневную награду!")
-                else:
-                    logger.error(f"Не смогли получить ежндневную награду! Код ответа сервера: {ClaimDailyRewards[1]}")
-                    logger.error(f"ответа сервера: {ClaimDailyRewards[0]}")
-            else:
-                logger.info(f"Ежедневная награда уже получена! | dailyRewardCounter.count = {user["user"]["counters"]["dailyRewardCounter"]["count"]} ")
             
             StartWork = core.command({"command":{"type":"StartWork"}}) #Пинаем программистов под зад!
             if StartWork[1] == 200:
-                logger.success(f"Стартовали работу!")   
-            else:        
-                logger.error(f"Не смогли стартовать работу! Код ответа сервера: {StartWork[1]}")
-                logger.error(f"ответа сервера: {StartWork[0]}")
+                logger.success(f"Стартовали работу!")            
+            
+            
             
             # Тут настройки выбора игры
             set_genre = "Arcade"
             set_setting = "Sports"
             
 
-            current_genre = user["user"]["game"]["genre"]
+            current_genre = user['user']['game']['genre']
             logger.info(f"Текущий жанр <green>{current_genre}</green> ")
             gameName,iconId = core.select_game(game_cfg,set_setting,set_genre) #Сгенерировали имя игры и получили iconId
             
             
            
             try:
-                if user["user"]["deferredRewards"][0]["reward"]["totalGames"] > 0:
+                if user['user']['deferredRewards'][0]['reward']['totalGames'] > 0:
                     # print("Ха ха! Есть чем поживиться!")
-                    logger.success(f"Игр выпущено! {user["user"]["deferredRewards"][0]["reward"]["totalGames"]} шт")
+                    logger.success(f"Игр выпущено! {user['user']['deferredRewards'][0]['reward']['totalGames']} шт")
                     Released = core.command({"command":{"type":"ClaimReleasedGamesRewards"}}) #Собираем ништяки
                     if Released[1] == 200:
                         logger.success(f"Сбор ништяков произведён успешно!")
-                        logger.success(f"{user["user"]["deferredRewards"][0]["reward"]["totalFunReward"][0]["type"]}: <fg #FFD700>{user["user"]["deferredRewards"][0]["reward"]["totalFunReward"][0]["amount"]}</fg #FFD700> | {user["user"]["deferredRewards"][0]["reward"]["totalFunReward"][1]["type"]}: <fg #FFD700>{user["user"]["deferredRewards"][0]["reward"]["totalFunReward"][1]["amount"]}</fg #FFD700> ")
+                        logger.success(f"{user['user']['deferredRewards'][0]['reward']['totalFunReward'][0]['type']}: <fg #FFD700>{user['user']['deferredRewards'][0]['reward']['totalFunReward'][0]['amount']}</fg #FFD700> | {user['user']['deferredRewards'][0]['reward']['totalFunReward'][1]['type']}: <fg #FFD700>{user['user']['deferredRewards'][0]['reward']['totalFunReward'][1]['amount']}</fg #FFD700> ")
             except (KeyError, IndexError, TypeError):
                 logger.info(f"Собирать нечего. :)")
 
