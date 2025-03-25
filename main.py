fcb7a69621b8d36ef073a5ac28ea4dbd172aaa00
Ilya <treyne@mail.ru>
2025-03-25 11:54:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,27 +14,57 @@ def main():
         try:
             logger.info(f"[--------->Начата новая иттерация<---------]")
             IP = core.IP()
-            logger.info(f"Ваш IP: <red>{IP["ip"]}</red> | Country: <blue>{IP["country_code"]}</blue> | City: <green>{IP["city_name"]}</green>")
+            logger.info(f"Ваш IP: <red>{IP['ip']}</red> | Country: <blue>{IP['country_code']}</blue> | City: <green>{IP['city_name']}</green>")
             AccountInfo = core.account_info()
-            logger.info(f"Ваш аккаунт ID: <blue>{AccountInfo["accountInfo"]["id"]}</blue> | Name: <blue>{AccountInfo["accountInfo"]["name"]}</blue> | at: <blue>{AccountInfo["accountInfo"]["at"]}</blue>")
+            logger.info(f"Ваш аккаунт ID: <blue>{AccountInfo['accountInfo']['id']}</blue> | Name: <blue>{AccountInfo['accountInfo']['name']}</blue> | at: <blue>{AccountInfo['accountInfo']['at']}</blue>")
             season2_config_version,user = core.sync()
 
-            logger.info(f"gamepads: <blue>{user["user"]["profile"]["gamepads"]}</blue> | gamepads Toltal: <blue>{user["user"]["profile"]["gamepads_total"]}</blue> | reputation: <blue>{user["user"]["profile"]["reputation"]}</blue>")
-            logger.info(f"soft money total: <fg #FFD700>{user["user"]["profile"]["soft_money_total"]}</fg #FFD700> | soft money: <fg #FFD700>{user["user"]["profile"]["soft_money"]}</fg #FFD700>")
-            logger.info(f"hard money total: <fg #FFD700>{user["user"]["profile"]["hard_money_total"]}</fg #FFD700> | hard money: <fg #FFD700>{user["user"]["profile"]["hard_money"]}</fg #FFD700>")
-            logger.info(f"Total Profit: <yellow>{user["user"]["statistics"]["totalProfit"]}</yellow> | totalFun: <yellow>{user["user"]["statistics"]["totalFun"]}</yellow>| totalGames: <yellow>{user["user"]["statistics"]["totalGames"]}</yellow>")
+            logger.info(f"gamepads: <blue>{user['user']['profile']['gamepads']}</blue> | gamepads Toltal: <blue>{user['user']['profile']['gamepads_total']}</blue> | reputation: <blue>{user['user']['profile']['reputation']}</blue>")
+            logger.info(f"soft money total: <fg #FFD700>{user['user']['profile']['soft_money_total']}</fg #FFD700> | soft money: <fg #FFD700>{user['user']['profile']['soft_money']}</fg #FFD700>")
+            logger.info(f"hard money total: <fg #FFD700>{user['user']['profile']['hard_money_total']}</fg #FFD700> | hard money: <fg #FFD700>{user['user']['profile']['hard_money']}</fg #FFD700>")
+            logger.info(f"Total Profit: <yellow>{user['user']['statistics']['totalProfit']}</yellow> | totalFun: <yellow>{user['user']['statistics']['totalFun']}</yellow>| totalGames: <yellow>{user['user']['statistics']['totalGames']}</yellow>")
             game_cfg = core.game_config(season2_config_version)
             if game_cfg:
                  logger.success(f"Файл конфигурации <green>{season2_config_version}.json</green> загружен")
+            
+            
+            StartWork = core.command({"command":{"type":"StartWork"}}) #Пинаем программистов под зад!
+            if StartWork[1] == 200:
+                logger.success(f"Стартовали работу!")            
+            
+            
+            
+            # Тут настройки выбора игры
+            set_genre = "Tycoon"
+            set_setting = "Sports"
+            
+
+            current_genre = user['user']['game']['genre']
+            logger.info(f"Текущий жанр <green>{current_genre}</green> ")
+            gameName,iconId = core.select_game(game_cfg,set_setting,set_genre) #Сгенерировали имя игры и получили iconId
+      
+            try:
+                for reward_data in user.get("user", {}).get("deferredRewards", []):
+                    reward = reward_data.get("reward", {})
+                    if reward.get("type") == "DeferredRewardGamesRelease":
+                        totalGames = reward.get('totalGames')
+                        totalFun = reward.get('totalFun')
+                if totalGames > 0:
+                    logger.success(f"Игр выпущено! {totalGames} шт | очков фана {totalFun}")
+                    Released = core.command({"command":{"type":"ClaimReleasedGamesRewards"}}) #Собираем ништяки
+                    if Released[1] == 200:
+                        logger.success(f"Сбор ништяков произведён успешно!")
+                        
+                        # logger.success(f"{user['user']['deferredRewards'][0]['reward']['totalFunReward'][0]['type']}: <fg #FFD700>{user['user']['deferredRewards'][0]['reward']['totalFunReward'][0]['amount']}</fg #FFD700> | {user['user']['deferredRewards'][0]['reward']['totalFunReward'][1]['type']}: <fg #FFD700>{user['user']['deferredRewards'][0]['reward']['totalFunReward'][1]['amount']}</fg #FFD700> ")
+                else:
+                    logger.info(f"Собирать нечего. :)")
+            except Exception as Err:
+                logger.info(f"Собирать нечего. :)")
 
             
-            
-            
-            
-            
-            
-            #################################Тут работа с ежедневной наградой###########################################################
-            
+
+
+            #################################Тут работа с ежедневной наградой###########################################################       
             updated_at_str = user["user"]["counters"]["dailyRewardCounter"]["updatedAt"] # Получаем строку с датой
             updated_at = datetime.strptime(updated_at_str, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=UTC) # Преобразуем её в объект datetime (указываем, что это UTC-время) 
             yesterday = (datetime.now(UTC) - timedelta(days=1)).date() # Определяем вчерашнюю дату в UTC
@@ -52,40 +82,7 @@ def main():
                 logger.info(f"Ежедневная награда уже получена! | dailyRewardCounter.count = {user["user"]["counters"]["dailyRewardCounter"]["count"]} ")
             #################################Закончили работу с ежедневной наградой###########################################################
             
-            
-            
-            
-            
-            StartWork = core.command({"command":{"type":"StartWork"}}) #Пинаем программистов под зад!
-            if StartWork[1] == 200:
-                logger.success(f"Стартовали работу!")   
-            else:        
-                logger.error(f"Не смогли стартовать работу! Код ответа сервера: {StartWork[1]}")
-                logger.error(f"ответа сервера: {StartWork[0]}")
-            
-            # Тут настройки выбора игры
-            set_genre = "Arcade"
-            set_setting = "Sports"
-            
 
-            current_genre = user["user"]["game"]["genre"]
-            logger.info(f"Текущий жанр <green>{current_genre}</green> ")
-            gameName,iconId = core.select_game(game_cfg,set_setting,set_genre) #Сгенерировали имя игры и получили iconId
-            
-            
-           
-            try:
-                if user["user"]["deferredRewards"][0]["reward"]["totalGames"] > 0:
-                    # print("Ха ха! Есть чем поживиться!")
-                    logger.success(f"Игр выпущено! {user["user"]["deferredRewards"][0]["reward"]["totalGames"]} шт")
-                    Released = core.command({"command":{"type":"ClaimReleasedGamesRewards"}}) #Собираем ништяки
-                    if Released[1] == 200:
-                        logger.success(f"Сбор ништяков произведён успешно!")
-                        logger.success(f"{user["user"]["deferredRewards"][0]["reward"]["totalFunReward"][0]["type"]}: <fg #FFD700>{user["user"]["deferredRewards"][0]["reward"]["totalFunReward"][0]["amount"]}</fg #FFD700> | {user["user"]["deferredRewards"][0]["reward"]["totalFunReward"][1]["type"]}: <fg #FFD700>{user["user"]["deferredRewards"][0]["reward"]["totalFunReward"][1]["amount"]}</fg #FFD700> ")
-            except (KeyError, IndexError, TypeError):
-                logger.info(f"Собирать нечего. :)")
-
-            
             
             
             ZoiPM = core.ZoiPM(set_genre,current_genre,iconId,set_setting,gameName)
@@ -102,7 +99,11 @@ def main():
             else:
                 logger.info(f"{dailyCiphers[0]}")
             print ("\n")
-            core.countdown_timer(random.randint(180, 355),'До следующего логина: ')
+            
+            # Тут будем собирать комбо
+            
+            
+            core.countdown_timer(random.randint(197, 200),'До следующего логина: ')
             time.sleep(3)
         except Exception as error:
             print(f'Ошибка {error}')
