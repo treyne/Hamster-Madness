5288913649f0315afdb292e6e0f966cfd75911f4
Ilya <treyne@mail.ru>
2025-04-08 16:50:29 +0300

    Update headers.py Добалена возможность работы с bitquest

--- headers.py
+++ headers.py
@@ -1,61 +1,58 @@
 import os
 from dotenv import load_dotenv
 
-
-
-def get_headers(Method):
+def get_headers(Method, BitQuest=False):
     # Загрузка переменных из .env файла
     load_dotenv()
     Bearer = os.getenv('Bearer')
-    Bearer_BitQuest = os.getenv('Bearer_BitQuest')    
+    Bearer_BitQuest = os.getenv('Bearer_BitQuest')
+
+    referer = "https://app.bitquest.games/" if BitQuest else "https://season2.hamsterkombatgame.io/"
+    token = Bearer_BitQuest if BitQuest else Bearer
+
+    common_headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36",
+        "Referer": referer,
+        "Connection": "keep-alive",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        "Priority": "u=4"
+    }
+
     if Method == "OPTION":
         return {
-            "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36",
             "Accept": "*/*",
             "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+            **common_headers,
             "Accept-Encoding": "gzip, deflate, br, zstd",
-            f"Access-Control-Request-Method": Method,
+            "Access-Control-Request-Method": Method,
             "Access-Control-Request-Headers": "authorization,content-type",
-            "Referer": "https://season2.hamsterkombatgame.io/",
-            "Connection": "keep-alive",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-site",
-            "Priority": "u=4"
         }
-        
-    if Method == "POST":
-        return {
-            "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36",
-            "Accept": "application/json",
-            "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Referer": "https://season2.hamsterkombatgame.io/",
-            "Authorization": f"Bearer {Bearer}",
-            "Content-Type": "application/json",
-            "Connection": "keep-alive",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-site",
-            "Priority": "u=4"
-        }
-        
-    if Method == "GET":
-        return {
-            "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36",
-            "Accept": "application/json",
-            "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Referer": "https://season2.hamsterkombatgame.io/",
-            "Authorization": f"Bearer {Bearer}",
-            "Content-Type": "application/json",
-            "Connection": "keep-alive",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-site",
-            "Priority": "u=4"
-        }
-    
-        
-      
 
+    if Method == "POST":
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36",
+            "Accept": "application/json",
+            "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Authorization": f"Bearer {token}",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "Priority": "u=4",
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
+            "Referer": referer
+        }
+        return headers
+
+    if Method == "GET":
+        headers = {
+            "Accept": "application/json",
+            "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+            **common_headers,
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {token}",
+        }
+        return headers
