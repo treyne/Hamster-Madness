1abc1baeee465351ac0eb52b1ef00c638d34d6ca
Ilya <treyne@mail.ru>
2025-04-09 18:57:25 +0300

    Update headers.py

--- headers.py
+++ headers.py
@@ -10,6 +10,12 @@ def get_headers(Method, BitQuest=False):
     referer = "https://app.bitquest.games/" if BitQuest else "https://season2.hamsterkombatgame.io/"
     token = Bearer_BitQuest if BitQuest else Bearer
 
+    if not BitQuest:
+        Type = {"Content-Type": "application/json"}
+    else:
+        Type = {}
+        
+    
     common_headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36",
         "Referer": referer,
@@ -17,7 +23,11 @@ def get_headers(Method, BitQuest=False):
         "Sec-Fetch-Dest": "empty",
         "Sec-Fetch-Mode": "cors",
         "Sec-Fetch-Site": "same-site",
-        "Priority": "u=4"
+        "Priority": "u=4",
+        **Type
+        
+
+            
     }
 
     if Method == "OPTION":
@@ -32,17 +42,14 @@ def get_headers(Method, BitQuest=False):
 
     if Method == "POST":
         headers = {
-            "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36",
             "Accept": "application/json",
             "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+            **common_headers,
             "Authorization": f"Bearer {token}",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-site",
-            "Priority": "u=4",
             "Pragma": "no-cache",
             "Cache-Control": "no-cache",
-            "Referer": referer
+             
+
         }
         return headers
 
